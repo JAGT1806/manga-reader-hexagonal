b8b178f24728bb75d475753ba233132ca19b219b
JAGT1806 <jagt1806@gmail.com>
2025-04-11 19:44:32 -0500

    initial commit

---
+++ src/main/java/com/jagt/mangareader/MangaReaderHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.jagt.mangareader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaReaderHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangaReaderHexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jagt/mangareader/MangaReaderHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagt.mangareader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaReaderHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
